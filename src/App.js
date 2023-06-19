ebaf1597ce7c333f5f8befe9ba269d2750e8f137
dmbbq <malenkaya269@gmail.com>
2023-06-19 17:57:59 +0300

    new changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import user from './user.json';
 import Profile from './components/Profile/Profile.jsx';
 import Statistics from './components/Statistics/Statistics.jsx';
-import data from './data.json';
 import FriendList from './components/FriendList/FriendList.jsx';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import user from './user.json';
+import data from './data.json';
 import friends from './friends.json';
+import transactions from './transaction.json'
 function App() {
   return (
     <div className="App">
@@ -16,6 +18,9 @@ function App() {
       stats={user.stats}/>
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <h1>Transaction History</h1>
+      <TransactionHistory items={transactions} />
+
     </div>
     
   );
