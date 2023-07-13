a02923f0995c1667cf4a76f73d9e8ecde72e3178
dmbbq <malenkaya269@gmail.com>
2023-07-13 13:28:47 +0300

    s

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -19,7 +19,7 @@ const Statistics = ({ title, stats }) => {
   );
 };
 Statistics.propTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
